b702ee421ef4aca88e65361e1005034050cbde0c
Damien Fernandes <damien.fernandes24@gmail.com>
2023-11-07 09:16:28 +0100

    remakr v4 not working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,21 @@
+import remarkGfm from 'remark-gfm'
+
 /** @type { import('@storybook/web-components-webpack5').StorybookConfig } */
 const config = {
   stories: [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials",  {
+    name: '@storybook/addon-docs',
+    options: {
+      mdxPluginOptions: {
+        mdxCompileOptions: {
+          remarkPlugins: [remarkGfm],
+        },
+      },
+    },
+  }],
   framework: {
     name: "@storybook/web-components-webpack5",
     options: {},
@@ -12,5 +23,5 @@ const config = {
   docs: {
     autodocs: "tag",
   },
-};
-export default config;
+}
+export default config
